7ba51ef0c70d5373f579cd0a0891c5f218e891d5
Chiki738 <carlosdalzamorah@gmail.com>
2025-05-04 16:59:13 -0500

    refactor: descomentar y organizar el código en varios componentes y estilos

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,70 +1,70 @@
-// import "../assets/styles/Footer.css";
-// import politicas from "../assets/pdf/politicas.pdf";
-// import terminos from "../assets/pdf/terminos.pdf";
+import "../assets/styles/Footer.css";
+import politicas from "../assets/pdf/politicas.pdf";
+import terminos from "../assets/pdf/terminos.pdf";
 
-// function Footer() {
-//   return (
-//     <footer className="bg-dark text-white py-3 gap-sm-3" >
-//       <div className="d-flex flex-sm-row flex-column justify-content-evenly align-items-center">
-//         <ul className="p-0 m-0">
-//           <li className="p-1">
-//             <a href={politicas} target="_blank" rel="noopener noreferrer">
-//               Política de Privacidad
-//             </a>
-//           </li>
-//           <li className="p-1">
-//             <a href={terminos} target="_blank" rel="noopener noreferrer">
-//               Términos y Condiciones
-//             </a>
-//           </li>
-//         </ul>
+function Footer() {
+  return (
+    <footer className="bg-dark text-white py-3 gap-sm-3">
+      <div className="d-flex flex-sm-row flex-column justify-content-evenly align-items-center">
+        <ul className="p-0 mb-4 mb-sm-0">
+          <li className="p-1">
+            <a href={politicas} target="_blank" rel="noopener noreferrer">
+              Política de Privacidad
+            </a>
+          </li>
+          <li className="p-1">
+            <a href={terminos} target="_blank" rel="noopener noreferrer">
+              Términos y Condiciones
+            </a>
+          </li>
+        </ul>
 
-//         <div className="redesEnlace d-flex justify-content-center flex-wrap">
-//           <a
-//             href="https://www.facebook.com/"
-//             target="_blank"
-//             title="Facebook"
-//             rel="noopener noreferrer">
-//             <i
-//               className="fa-brands fa-facebook rounded-2 p-1"
-//               id="facebook"></i>
-//           </a>
+        <div className="redesEnlace d-flex justify-content-center flex-wrap mb-4 mb-sm-0">
+          <a
+            href="https://www.facebook.com/"
+            target="_blank"
+            title="Facebook"
+            rel="noopener noreferrer">
+            <i
+              className="fa-brands fa-facebook rounded-2 p-1"
+              id="facebook"></i>
+          </a>
 
-//           <a
-//             href="https://www.instagram.com/"
-//             title="Instagram"
-//             target="_blank"
-//             rel="noopener noreferrer">
-//             <i
-//               className="fa-brands fa-instagram rounded-2 p-1"
-//               id="instagram"></i>
-//           </a>
+          <a
+            href="https://www.instagram.com/"
+            title="Instagram"
+            target="_blank"
+            rel="noopener noreferrer">
+            <i
+              className="fa-brands fa-instagram rounded-2 p-1"
+              id="instagram"></i>
+          </a>
 
-//           <a
-//             href="https://x.com/"
-//             title="Twitter"
-//             target="_blank"
-//             rel="noopener noreferrer">
-//             <i
-//               className="fa-brands fa-x-twitter rounded-2 p-1"
-//               id="twitter"></i>
-//           </a>
+          <a
+            href="https://x.com/"
+            title="Twitter"
+            target="_blank"
+            rel="noopener noreferrer">
+            <i
+              className="fa-brands fa-x-twitter rounded-2 p-1"
+              id="twitter"></i>
+          </a>
 
-//           <a
-//             href="https://mail.google.com"
-//             title="Gmail"
-//             target="_blank"
-//             rel="noopener noreferrer">
-//             <i className="fa-regular fa-envelope rounded-2 p-1" id="gmail"></i>
-//           </a>
-//         </div>
-//       </div>
+          <a
+            href="https://mail.google.com"
+            title="Gmail"
+            target="_blank"
+            rel="noopener noreferrer">
+            <i className="fa-regular fa-envelope rounded-2 p-1" id="gmail"></i>
+          </a>
+        </div>
+      </div>
 
-//       <p className="text-center pt-2">
-//         © 2025 CineFlix. Todos los derechos reservados.
-//       </p>
-//     </footer>
-//   );
-// }
+      <p className="text-center pt-2">
+        © 2025 CineFlix. Todos los derechos reservados.
+      </p>
+    </footer>
+  );
+}
 
-// export default Footer;
+export default Footer;
--- src/layouts/LandingLayout.jsx
+++ src/layouts/LandingLayout.jsx
@@ -1,23 +1,23 @@
-// import { Link } from "react-router-dom";
-// import Header from "../components/Header.jsx";
-// import Landing from "../pages/Landing.jsx";
-// import Footer from "../components/Footer.jsx";
-// import "../assets/styles/LandingLayout.css";
+import { Link } from "react-router-dom";
+import Header from "../components/Header.jsx";
+import Landing from "../pages/Landing.jsx";
+import Footer from "../components/Footer.jsx";
+import "../assets/styles/LandingLayout.css";
 
-// function LandingLayout() {
-//   return (
-//     <>
-//       <Header redirectTo="/">
-//         <Link to="/Login">
-//           <button className="px-sm-4 py-sm-1 px-2 text-dark fw-bold btnLogin rounded-3 border-success">
-//             Iniciar sesión
-//           </button>
-//         </Link>
-//       </Header>
-//       <Landing />
-//       <Footer />
-//     </>
-//   );
-// }
+function LandingLayout() {
+  return (
+    <>
+      <Header redirectTo="/">
+        <Link to="/Login">
+          <button className="btnLogin px-sm-4 py-sm-1 px-2 text-dark fw-bold rounded-3 border-success">
+            Iniciar sesión
+          </button>
+        </Link>
+      </Header>
+      <Landing />
+      <Footer />
+    </>
+  );
+}
 
-// export default LandingLayout;
+export default LandingLayout;
