057cd2d06f631e562aabf3a8ef72e27236627786
Chiki738 <carlosdalzamorah@gmail.com>
2025-05-05 23:09:17 -0500

    refactor: descomentar el código en Login y mejorar la estructura del formulario

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,50 +1,45 @@
-// import { useState } from "react";
-// import { useNavigate } from "react-router-dom";
-// import { loginUser } from "../services/authService";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "../assets/styles/Login.css";
 
 function Login() {
-//   const [email, setEmail] = useState("");
-//   const [password, setPassword] = useState("");
-//   const [showPassword, setShowPassword] = useState(false);
-//   const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const navigate = useNavigate();
 
-//   const handleLogin = async (e) => {
-//     e.preventDefault();
-//     try {
-//     //   const data = await loginUser(email, password);
-//       localStorage.setItem("token", data.token);
-//       localStorage.setItem("user", JSON.stringify(data.user));
-//       navigate("/Home");
-//     } catch (error) {
-//       alert("Credenciales incorrectas");
-//       console.error("Error de inicio de sesión:", error);
-//     }
-//   };
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      //   const data = await loginUser(email, password);
+      navigate("/Home");
+    } catch (error) {
+      alert("Credenciales incorrectas");
+      console.error("Error de inicio de sesión:", error);
+    }
+  };
 
-//   const togglePassword = () => setShowPassword(!showPassword);
+  const togglePassword = () => setShowPassword(!showPassword);
 
   return (
     <div className="formLogin d-flex flex-column justify-content-center align-items-center text-white text-center">
       <form
-        // onSubmit={handleLogin}
+        onSubmit={handleLogin}
         className="d-flex flex-column justify-content-center align-items-center p-4 rounded-2">
         <h3>INISIAR SESIÓN</h3>
-
-        {/* <div className="mb-3">
+        <div className="mb-3">
           <input
             type="email"
             placeholder="Ingresar correo electrónico"
             className="form-control"
             style={{ width: "300px" }}
             id="email"
-            // value={email}
-            // onChange={(e) => setEmail(e.target.value)}
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
             required
           />
-        </div> */}
-
-        {/* <div className="input-group mb-3">
+        </div>
+        <div className="input-group mb-3">
           <input
             type={showPassword ? "text" : "password"}
             className="form-control"
@@ -54,7 +49,7 @@ function Login() {
             onChange={(e) => setPassword(e.target.value)}
             required
           />
-          <button
+          <link
             className="btn btn-outline-secondary"
             type="button"
             onClick={togglePassword}>
@@ -62,21 +57,20 @@ function Login() {
               className={`fa-solid ${
                 showPassword ? "fa-eye-slash" : "fa-eye"
               }`}></i>
-          </button>
-        </div> */}
-
-        {/* <button
+          </link>
+        </div>
+        /*{" "}
+        <button
           type="submit"
           className="btn btn-primary px-4 py-1 text-dark fw-bold btnLogin rounded-3 border-success">
           INGRESAR
         </button>
-
         <p className="pb-0">
           ¿No tienes cuenta?&nbsp;
           <a href="/Registro" style={{ color: "white" }}>
             <strong>Registrate</strong>
           </a>
-        </p> */}
+        </p>
       </form>
     </div>
   );
